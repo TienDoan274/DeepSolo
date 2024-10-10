eb8b9dc8a8fe7f297fa43389d837023915b2b441
TienDoan274 <doannhattien4@gmail.com>
2024-10-10 22:01:22 +0700

    a

--- DeepSolo++/demo/demo.py
+++ DeepSolo++/demo/demo.py
@@ -85,7 +85,8 @@ if __name__ == "__main__":
             img = read_image(path, format="BGR")
             start_time = time.time()
             predictions, visualized_output = demo.run_on_image(img)
-            print(predictions['sem_seg'])
+            if("sem_seg" in predictions):
+                print(predictions['sem_seg'])
     elif args.webcam:
         assert args.input is None, "Cannot have both --input and --webcam!"
         cam = cv2.VideoCapture(0)
